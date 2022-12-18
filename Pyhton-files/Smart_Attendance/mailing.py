a0fa45f53840d3c61f640f6d90dce0238ca2f364
Ankit <50232341+imankit007@users.noreply.github.com>
2022-12-18 21:45:02 +0530

    edited mailing.py

--- Pyhton-files/Smart_Attendance/mailing.py
+++ Pyhton-files/Smart_Attendance/mailing.py
@@ -1,20 +1,43 @@
 import smtplib
 import pymongo
+from twilio.rest import Client
+from email.message import EmailMessage
 
 
-s.login("ankit6202273766@gmail.com","kpdtkxyzupafsyac")
+account_sid = 'ACee9209e9f050daf64e7866b49d8e2a78'
+auth_token = '514cc260b9638663d4ef701440b5921a'
+
+
 def sendMail(absent, course, today):
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
     s.login("ankit6202273766@gmail.com", "kpdtkxyzupafsyac")
+    client = Client(account_sid, auth_token)
     myclient = pymongo.MongoClient("mongodb+srv://ankit:attendance@cluster0.iofnken.mongodb.net/test")
     mydb = myclient["Student_Database"]
     mycol = mydb["CSE_5_A"]
+
     for USN in absent:
+        msg = EmailMessage()
+        msg['From'] = 'ankit6202273766@gmail.com'
+        msg['subject'] = "Absent in class"
         student = mycol.find_one({"USN":USN})
         name = student.get("Name")
         mail=student.get("Email")
         phone = student.get("Mobile")
+        msg['To']= mail;
         message = f"Dear parent, your ward {name} was absent for {course} class on {today}."
-        s.sendmail("ankit6202273766+sender@gmail.com", mail, message)
+        msg.set_content(message)
+        # client.messages.create(
+        #     from_='whatsapp:+14155238886',
+        #     body=message,
+        #     to=f'whatsapp:{phone}'
+        # )
+        s.send_message(msg)
+        #
+        # s.sendmail("ankit6202273766+sender@gmail.com", mail, message)
     s.quit()
+
+
+
+sendMail(['2SD20CS014','2SD20CS017'], "CDSS", "18/12/2022")
