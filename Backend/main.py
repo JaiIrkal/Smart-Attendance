5eae1c1ecac43fc7f85e5b03cf65b9e4a70f1c84
Ankit <50232341+imankit007@users.noreply.github.com>
2023-03-27 16:21:43 +0530

    added dummy data
    This allows us to work  on the visual element of the website without the need for internet access.

--- Backend/main.py
+++ Backend/main.py
@@ -1,74 +1,68 @@
-from fastapi import FastAPI
-import pymongo
-import json
+from typing import List
 
+from fastapi import FastAPI
 from pydantic import BaseModel, Field, EmailStr
-from bson import ObjectId
-from typing import Optional, List
 import motor.motor_asyncio
 
 ACADEMIC_YEAR = "2022-2023"
 
 app = FastAPI()
 
-mongodb_client = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://ankit:attendance@cluster0.iofnken.mongodb.net/test")
-#this database conatains the attendance data
+mongodb_client = motor.motor_asyncio.AsyncIOMotorClient(
+    "mongodb+srv://ankit:attendance@cluster0.iofnken.mongodb.net/test")
+# this database conatains the attendance data
 attendancedb = mongodb_client.Attendance_Database
 
 # this database contains details about the classes
-ClassDb  = mongodb_client.Class_Database
+ClassDb = mongodb_client.Class_Database
 
-#this database contains details about the timetable of classes
+# this database contains details about the timetable of classes
 TimeTableDb = mongodb_client.timetable
 
-#this points to the collection of Students in the Database
+# this points to the collection of Students in the Database
 studentsCollection = attendancedb["Students"]
 
-#this points to the collection of Teachers in the database
+# this points to the collection of Teachers in the database
 teacherCollection = attendancedb["Teachers"]
 
-#this points to the collection of classes in current academic year
+# this points to the collection of classes in current academic year
 classCollection = ClassDb[ACADEMIC_YEAR]
-
 
 
 @app.get("/student/{USN}")
 async def detailsOfStudent(USN):
-    student = await studentsCollection.find_one({"USN" : USN})
+    student = await studentsCollection.find_one({"USN": USN})
     className = f'{student["Branch"]}_{student["Sem"]}_{student["Div"]}'
-
     # this line finds the record for the class of the student
-    classDetails = await classCollection.find_one({"Branch":f'{student["Branch"]}', "Semester":int(f'{student["Sem"]}'),"Division" : f'{student["Div"]}'})
+    classDetails = await classCollection.find_one(
+        {"Branch": f'{student["Branch"]}', "Semester": int(f'{student["Sem"]}'), "Division": f'{student["Div"]}'})
     courses = classDetails["Courses_available"]
-
     AttendanceDetails = []
     for course in courses:
-        attendance= {
-            "Subject_Name" : classDetails[f"{course}"]["Course_abbrv"],
+        attendance = {
+            "Subject_Name": classDetails[f"{course}"]["Course_abbrv"],
             "Classes_conducted": classDetails[f"{course}"]["Classes_conducted"],
             "Attendance": student[f"Sem_{student['Sem']}"][f"{course}_attendance"]
         }
         AttendanceDetails.append(attendance)
-
     studentDetails = {"Name": student["Name"],
                       "USN": student["USN"],
-                    "Class": className,
+                      "Class": className,
                       "Branch": student["Branch"],
                       "Semester": student["Sem"],
                       "Division": student["Div"],
-                    "AttendanceData": AttendanceDetails
+                      "AttendanceData": AttendanceDetails
                       }
     return studentDetails
 
 
 @app.get("/teacher/{teacher_id}")
 async def teacher(teacher_id):
-
     classesData = []
     teacher = await teacherCollection.find_one({"ID": teacher_id})
     branch = teacher["Branch"]
     for classTaught in teacher["Classes"]:
-        Semester  = classTaught["Semester"]
+        Semester = classTaught["Semester"]
         Division = classTaught["Division"]
         classData = await classCollection.find_one({"Branch": branch, "Semester": Semester, "Division": Division})
         listofSubject = []
@@ -79,7 +73,7 @@ async def teacher(teacher_id):
             for student in classData["Students"]:
                 studentData = await studentsCollection.find_one({"USN": student})
 
-                studentAttendanceData= {
+                studentAttendanceData = {
                     "USN": student,
                     "Name": studentData["Name"],
                     "Attendance": studentData[f"Sem_{Semester}"][f"{subject}_attendance"]
@@ -104,7 +98,14 @@ async def teacher(teacher_id):
         "Name": teacher["Name"],
         "Classes": classesData,
     }
-
     return teacherData
 
 
+
+
+@app.post("/sendwarning")
+def sendWarning(item: List):
+
+    for item in list:
+        print(item)
+    return {"message": "Post Successful"}
--- Flask Website/main.py
+++ Flask Website/main.py
@@ -16,7 +16,7 @@ from wtforms_alchemy import PhoneNumberField
 
 #app configs
 app = Flask(__name__, template_folder="templates")
-app.config['SECRET_KEY']="my ssecret key"
+app.config['SECRET_KEY']="my secret key"
 app.config['UPLOADED_IMAGES_DEST'] = 'uploads/images'
 
 images = UploadSet('images',IMAGES)
@@ -25,31 +25,33 @@ configure_uploads(app, images)
 
 API_BASE_URL = "http://127.0.0.1:8000"
 
+import dummydata
+
 #create a form for adding a student
 class StudentForm(FlaskForm):
     name= StringField('Name of the Student', validators=[DataRequired()])
     USN = StringField('USN',validators=[DataRequired()])
-    branch = SelectField('Branch', choices=[("CSE",'CSE'),('ISE','ISE')])
+    branch = SelectField('Branch', choices=[("CSE",'CSE'),('ISE','ISE'),('ECE', 'ECE'),('EEE','EEE'),('ME','ME'),('CE','CE')])
     semester = SelectField('Semester', choices=[(1),(2),(3),(4),(5),(6),(7),(8)])
     division = SelectField("Division", choices=[('A'),('B')])
     email = StringField("Email", validators=[ (DataRequired()), (validators.Email("Please enter your email address."))])
     mobile = StringField( "Phone Number",validators=[ DataRequired()])
 
 #database connections
-
-mongodb_client = pymongo.MongoClient("mongodb+srv://jai:attendance@cluster0.iofnken.mongodb.net/test")
-
-#student database
-studentdb = mongodb_client["Student_Database"]
-
-#teacher database
-classdb = mongodb_client["Class_Database"]
-
-teacherdb = mongodb_client["Staff_Database"]
-
-
-#login collection
-logins = studentdb["Students"]
+#
+# mongodb_client = pymongo.MongoClient("mongodb+srv://jai:attendance@cluster0.iofnken.mongodb.net/test")
+#
+# #student database
+# studentdb = mongodb_client["Student_Database"]
+#
+# #teacher database
+# classdb = mongodb_client["Class_Database"]
+#
+# teacherdb = mongodb_client["Staff_Database"]
+#
+#
+# #login collection
+# logins = studentdb["Students"]
 
 
 teacher_dict = {"Umakant Kulkarni":"CDSS",    "Rashmi_Athnikar":"SE",    "Anand_Vaidya":"DBMS",    "Yashoda_Sambrani":"MEPIP",
@@ -73,39 +75,32 @@ def home():
 
 #This is for adding a student
 assets_dir = os.path.join(os.path.dirname(app.instance_path), 'assets')
-@app.route('/addstudent',methods = ['GET','POST'])
-def addstudent():
-    name = None
-    studentform = StudentForm()
-    if request.method == 'POST' and studentform.is_submitted():
-        name=request.form['name']
-        USN = studentform.USN.data
-        email = studentform.email.data
-        mobile = studentform.mobile.data
-        branch = studentform.branch.data
-        semester = studentform.semester.data
-        division = studentform.division.data
-        dob = request.form['dob']
-        f = request.files['file']
-        f.save(os.path.join("./uploads/", secure_filename(f.filename)))
-        encode= faceEncodings(f.filename)
-        addstudentToDatabase(USN,name,email,mobile,branch,semester,division,encode.tolist(),dob)
-        return 'Student Added'
-    return render_template("addStudent.html",form=studentform)
+# @app.route('/addstudent',methods = ['GET','POST'])
+# def addstudent():
+#     name = None
+#     studentform = StudentForm()
+#     if request.method == 'POST' and studentform.is_submitted():
+#         name=request.form['name']
+#         USN = studentform.USN.data
+#         email = studentform.email.data
+#         mobile = studentform.mobile.data
+#         branch = studentform.branch.data
+#         semester = studentform.semester.data
+#         division = studentform.division.data
+#         dob = request.form['dob']
+#         f = request.files['file']
+#         f.save(os.path.join("./uploads/", secure_filename(f.filename)))
+#         encode= faceEncodings(f.filename)
+#         addstudentToDatabase(USN,name,email,mobile,branch,semester,division,encode.tolist(),dob)
+#         return 'Student Added'
+#     return render_template("addStudent.html",form=studentform)
 
 # This route is for student login
 @app.route('/studentlogin', methods = ["GET", "POST"])
 def student_login():
-    logintable = studentdb['Student_Logins']
+    # logintable = studentdb['Student_Logins']
     if(request.method == 'POST'):
-        usn = request.form['usn']
-        passw = request.form['passw']
-        user = logintable.find_one({"USN": f"{usn}"})
-        if(usn is not None):
-            if(user.get("PASSW") == passw):
-                session['loggedin'] = True
-                session['id'] = usn
-                session['class']= user.get("Class")
+
                 return redirect( url_for("student_page"))
     return render_template("student_login.html")
 
@@ -113,38 +108,33 @@ def student_login():
 @app.route('/teacherlogin', methods = ["GET", "POST"])
 def teacher_login():
 
-    teachercol = teacherdb["Teachers"]
+    # teachercol = teacherdb["Teachers"]
     if request.method == "POST":
-        uid  =request.form['tid']
-        passw = request.form['psw']
-        teacherdata = teachercol.find_one({"UserId": uid})
-        if(teacherdata is not None):
-            if(teacherdata.get("PASSW")== passw):
-                session['loggedin'] = True
-                session['id'] = uid
+
                 return redirect( url_for("teacher_page"))
     return render_template("teacher_login.html")
 
 @app.route('/teacher',methods = ["GET"])
 def teacher_page():
 
-    ID = "teacher001"
-    url = f"{API_BASE_URL}/teacher/{ID}"
-    response = urllib.request.urlopen(url)
-    Data = json.loads(response.read())
-    return render_template("teacher.html", Data = Data )
-
+    # ID = "teacher001"
+    # url = f"{API_BASE_URL}/teacher/{ID}"
+    # response = urllib.request.urlopen(url)
+    # response = dummydata.teacherData
+    # Data = json.loads(response.read())
+    return render_template("teacher.html", Data = dummydata.teacherData )
 
 
 
 @app.route('/student', methods = ["GET"])
 def student_page():
     if(request.method=="GET"):
-        usn = '2SD20CS017'
-        url = f"{API_BASE_URL}/student/{usn}"
-        response = urllib.request.urlopen(url)
-        studentDetails = json.loads(response.read())
-        return render_template("student.html",studentDetails=studentDetails )
+        # usn = '2SD20CS017'
+        # url = f"{API_BASE_URL}/student/{usn}"
+        # response = urllib.request.urlopen(url
+        # response = dummydata.studentData
+        # studentDetails = json.loads(response.read())
+        return render_template("student.html",studentDetails=dummydata.studentData )
 
 
 if __name__ == '__main__':
